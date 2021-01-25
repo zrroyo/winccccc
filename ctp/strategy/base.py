bb985960848ba39f7edc1b6acfe1b918c40d0533
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2021-01-26 00:22:17 +0800

    ctp:strategy:base: implement the Strategy class
    Fill the strategy.base module.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- ctp/strategy/base.py
+++ ctp/strategy/base.py
@@ -1,20 +1,75 @@
 # -*- coding:utf-8 -*-
 
+import os
+import traceback
+import numpy as np
 from abc import ABCMeta, abstractmethod
-from ..error import StrategyError
+from lib import GenConfig
+from ..globals import GlobalConfig
+from ..error import StrategyError, TradeDetailsRecordError
+from ..data import TradeDataComposition as TDC, TradeDetailsRecord as TDR
 
 
 class Strategy:
     """交易策略基类"""
     __metaclass__ = ABCMeta
+    # 交易信号
+    SIG_TRADE_SHORT = -1  # 卖空
+    SIG_TRADE_LONG = 1  # 买多
+    SIG_TRADE_NONE = 0  # 没有行情
+
+    def __init__(self, api, symbol, params, logger):
+        """
+        :param TqApi api:
+        :param string symbol:
+        :param string params: 策略参数字符串，参数间以';'号分隔
+        :param logger:
+        :raise StrategyError: 出错统一抛出StrategyError异常
+        """
+        self.api = api
+        self.symbol = symbol
+        self.params = params
+        self.logger = logger
+
+        self.tdc_file = self.get_history_data_file()
+        if not os.path.exists(self.tdc_file):
+            raise StrategyError(f"未找到 {symbol} 对应的历史数据！")
+        self.tdc = TDC(self.tdc_file, self.logger)
+
+        self.tdr_file = self.get_trade_details_file()
+        if not os.path.exists(self.tdr_file):
+            raise StrategyError(f"未找到 {symbol} 对应的历史数据！")
+
+        self.tdr = self.init_tdr()
+        if self.tdr is None:
+            raise StrategyError(f"TDR未初始化成功！")
+
+        self.attrs = self.parse_parameters(params)
+        if self.attrs is None:
+            raise StrategyError(f"解析策略参数错误 {params}！")
+
+        # 子策略必须要初始化自己的监听对象
+        self._listener = None
+
+    def init_tdr(self):
+        """初始化tdr ---
+        策略类可以重载该方法并返回自定义的TDR对象
+        :return: 正常返回TDR或其子类，失败则返回None
+        """
+        try:
+            ret = TDR(self.tdr_file, self.logger)
+        except TradeDetailsRecordError as e:
+            self.logger.error(f"初始化TDR错误：{traceback.format_exc(e)}")
+            ret = None
+        return ret
 
     @abstractmethod
-    def __init__(self, params):
+    def parse_parameters(self, params):
+        """解析交易参数
+        :param params:
+        :return:
         """
-        :param string params: 策略参数字符串，参数间以';'号分隔
-        :raise StrategyError: 出错统一抛出 StrategyError 异常
-        """
-        self._listener = None  # 子策略必须要初始化自己的监听对象
+        pass
 
     @property
     def active_listener(self):
@@ -34,3 +89,76 @@ class Strategy:
                     = 0: 持仓不变
         """
         pass
+
+    def get_history_data_file(self):
+        """得到历史数据文件"""
+        _global = GlobalConfig()
+        filename = self.symbol.rstrip('.csv')
+        return os.path.join(_global.get_md_runtime_dir(), filename)
+
+    def get_trade_details_file(self):
+        """得到交易详情文件"""
+        _global = GlobalConfig()
+        filename = self.symbol.rstrip('.csv')
+        return os.path.join(_global.get_trade_details_dir(), filename)
+
+    def get_signal_direction(self, signal):
+        """交易信号转换成方向字符串
+        :param signal: 交易信号
+        :return: 信号字串
+        """
+        ret = None
+        if signal == Strategy.SIG_TRADE_SHORT:
+            ret = 'Short'
+        elif signal == Strategy.SIG_TRADE_LONG:
+            ret = 'Long'
+        return ret
+
+    def gen_cfr(self, price, opPrice, direction):
+        """计算当前浮动利润率
+        :param price: 当前价格
+        :param direction: 方向
+        :param opPrice: 加仓价格
+        :return: 浮动利润率|NaN
+        """
+        ret = np.nan
+        try:
+            if direction == Strategy.SIG_TRADE_LONG:
+                ret = (price - opPrice) / opPrice
+            elif direction == Strategy.SIG_TRADE_SHORT:
+                ret = (opPrice - price) / opPrice
+            else:
+                raise StrategyError(f"未识别的交易信号 {direction}")
+        except ZeroDivisionError:
+            self.logger.error(f"输入opPrice异常 {opPrice}")
+        return ret
+
+
+class StrategyConfig(GenConfig):
+    """交易策略配置信息接口"""
+    def __init__(self, cfgFile, task_id):
+        """
+        :param cfgFile: 配置文件名
+        :param task_id: 合约id
+        """
+        super(StrategyConfig, self).__init__(cfgFile)
+        self.cfgFile = cfgFile
+        self.defaultSec = task_id
+
+    def get_strategy(self):
+        return self.getSecOption(self.defaultSec, 'strategy')
+
+
+class Position:
+    """持仓单位（仓位）"""
+    def __init__ (self, price, time, volume, direction):
+        """加仓信息
+        :param price: 成交价
+        :param time: 时间
+        :param volume: 开仓手数
+        :param direction: 方向
+        """
+        self.price = price
+        self.time = time
+        self.volume = volume
+        self.direction = direction
