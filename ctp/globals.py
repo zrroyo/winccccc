55785aec9d6927fab1e1dc754f52279a95e1876c
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2021-01-26 00:21:22 +0800

    ctp: introduce globals module
    Enable to access the global ctp configurations.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

---
+++ ctp/globals.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+import os
+from lib import GenConfig
+
+# 定义全局变量
+CTP_CONFIG_DIR = os.path.join(os.environ.get('HOME'), '.winctp')
+GLOBAL_CONFIG = os.path.join(CTP_CONFIG_DIR, 'global')
+TASKS_CONFIG = os.path.join(CTP_CONFIG_DIR, 'tasks')
+
+
+class GlobalConfig(GenConfig):
+    """读取全局配置信息"""
+    def __init__(self, cfgFile=GLOBAL_CONFIG):
+        super(GlobalConfig, self).__init__(cfgFile)
+        self.cfgFile = cfgFile
+        self.defaultSec = 'globals'
+
+    def get_md_runtime_dir(self):
+        return self.getSecOption(self.defaultSec, 'md_runtime_dir')
+
+    def get_trade_details_dir(self):
+        return self.getSecOption(self.defaultSec, 'trade_details_dir')
+
+    def get_trader_config_dir(self):
+        return self.getSecOption(self.defaultSec, 'trader_config_dir')
