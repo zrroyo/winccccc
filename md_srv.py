9a4c7f49df8c6e4de4bca6ab623d48e904246c1a
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2021-09-05 23:25:10 +0800

    md_srv: enable to set debug level by global config
    Enable to set debug level by global config.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- md_srv.py
+++ md_srv.py
@@ -9,10 +9,8 @@ import time
 import ctp
 sys.path.append(os.path.dirname(__file__))
 
-LOG_LEVEL = logging.DEBUG
 
-
-def get_logger(level):
+def get_logger():
     """初始化日志接口"""
     log_file = "%s.log" % time.strftime("%Y%m%d%H%M%S", time.localtime())
     _global_cfg = ctp.GlobalConfig()
@@ -20,7 +18,8 @@ def get_logger(level):
     if log_dir:
         log_file = os.path.join(log_dir, 'md', log_file)
     logger = logging.getLogger('ctp_srv')
-    logger.setLevel(level)
+    dbg_level = _global_cfg.get_mdsrv_debug_level()
+    logger.setLevel(dbg_level)
     fh = logging.FileHandler(log_file)
     fh.setFormatter(
         logging.Formatter('%(asctime)s:%(filename)s:%(funcName)s:%(lineno)d: <%(levelname)s> %(message)s')
@@ -29,7 +28,7 @@ def get_logger(level):
     return logger
 
 
-logger = get_logger(LOG_LEVEL)
+logger = get_logger()
 try:
     srv = ctp.CtpSrvMD(logger)
     srv.run()
